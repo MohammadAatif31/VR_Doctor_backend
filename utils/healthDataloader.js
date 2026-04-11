377717df258e770ab76d6776d3888d8f3f1fdc92
Mohammad Aatif <mdaatifa305@gmail.com>
2026-04-12 00:50:34 +0530

    fix: correct file path foe render

--- utils/healthDataloader.js
+++ utils/healthDataloader.js
@@ -1,7 +1,14 @@
 import fs from "fs";
 import path from "path";
+import { fileURLToPath } from "url";
 
-const filePath = path.resolve("./data/healthData.json");
+// __dirname fix for ES module
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-export const healthData =
-JSON.parse(fs.readFileSync(filePath,"utf-8"));
+// correct path
+const filePath = path.join(__dirname, "../data/healthData.json");
+
+export const healthData = JSON.parse(
+  fs.readFileSync(filePath, "utf-8")
+);
